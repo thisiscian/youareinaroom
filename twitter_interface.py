96b37cee287b95e220971206891479ac1b1f823e
Fionn Fitzmaurice <iamfionn@github.com>
2015-02-16 21:28:34 +0000

    DMs for help

--- twitter_interface.py
+++ twitter_interface.py
@@ -3,27 +3,33 @@
 
 import tweepy
 
-def connect():
+consumer = open("consumer.txt", "r").read().splitlines()
+access = open("access.txt", "r").read().splitlines()
 
-    consumer = open("consumer.txt", "r").read().splitlines()
-    access = open("access.txt", "r").read().splitlines()
+consumer_key = consumer[0]
+consumer_secret = consumer[1]
 
-    consumer_key = consumer[0]
-    consumer_secret = consumer[1]
+access_token = access[0]
+access_token_secret = access[1]
 
-    access_token = access[0]
-    access_token_secret = access[1]
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
 
-    api = tweepy.API(auth)
+#https://github.com/tweepy/tweepy/issues/554
+user = api.get_user(screen_name = "youareinaroom")
 
-    #https://github.com/tweepy/tweepy/issues/554
-    user = api.get_user(screen_name = "youareinaroom")
+def dm_tweet_to_admins(tweet_id):
+    admins = open("admins.txt", "r")
+    
+    for line in admins:
+        api.send_direct_message(user = line, text = "Help me with tweet_id " + str(tweet_id))
+
 
 if __name__ == "__main__":
-    
-    connect()
+
+    dm_tweet_to_admins("123")    
+
 
 
