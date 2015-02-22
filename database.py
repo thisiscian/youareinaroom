1c51edc71fa58575d8d269ea779646f9d28d2b7e
Cian Booth <this.is.cian@gmail.com>
2015-02-22 18:42:01 +0000

    implemented required database methods for main to run

--- database.py
+++ database.py
@@ -7,115 +7,36 @@ class Database:
 		self.__cursor.execute('.tables ?', table_name);
 		return self.__cursor.fetchall()
 	def __create_table(self, table_name, values):
-		command='create table if not exists %s (%s)' % ( table_name, ', '.join([ item[0]+' '+(item[1] if item[1] else 'text') for item in values.items() ]))
+		command='create table if not exists %s (%s)' % ( table_name, values)
 		self.__cursor.execute(command)
-	def __init__(self, name, initial_state):
-		self.initial_state=initial_state
+	def __init__(self, name):
 		self.__connection=sqlite3.connect(name)
 		self.__cursor=self.__connection.cursor()
-		self.__create_table('users',{'user_id':'text','state':'text'})
-		self.__create_table('interactions',{'required_state':'text','state_change':'text','message':'text'})
-		self.__create_table('commands',{'command':'text', 'interaction_id':'int'})
+		self.__create_table('users','user_id,state')
+		self.__create_table('interactions','required_state,state_change,messages')
+		self.__create_table('commands','command,interaction_ids')
 		self.__connection.commit()
+	def find_state(self, user_id):
+		self.__cursor.execute('select state from users where user_id=?', (user_id,))
+		result=self.__cursor.fetchone()
+		if not result:
+			return None
+		else:
+			return result[0]
 	def add_user(self, user_id):
-		pass
-	def link_statement_to_interaction(self, statement, interaction_id):
-		pass
-	def add_interaction(self):
-		pass
-	def find_interaction(self):
-		pass
-	def find_user(self):
-		pass
-	
-#import os
-#import sqlite3
-#import cmd
-#from collections import OrderedDict
-
-#database=Database('test.db',{})
-
-#def parse_variable(variable):
-#  match=var_regex.findall(variable)
-#  if len(match)==1:
-#    return match[0];
-#  elif len(match)==1:
-#    raise Exception("parse_variable didn't find any matches to the variable "+variable)
-#  else:
-#    raise Exception('parse_variable: should only find one match to variable, found multiple('+len(match)+') in '+variable)
-#
-#def search(variable):
-#  try:
-#    (table,conditions,returns)=parse_variable(variable)
-#  except:
-#    return []
-#  wheres=dict([(i[0],i[1]) for i in re.findall('(.+?)=(.+?)(,|$)', conditions)])
-#  if not returns: returns='*'
-#  return get_element(table,returns,wheres)
-#
-#
-#
-#def create_table(cursor, table_name, values):
-#	exstr='create table %s (%s)' % ( table_name, ', '.join([ item[0]+' '+(item[1] if item[1] else 'text') for item in values.items() ]))
-#	print(exstr)
-#	cursor.execute(exstr)
-#
-#def add_element_to_table(cursor,table_name,values):
-#	exstr='insert into %s values (?%s)' % (table_name, ',?'*(len(values)-1))
-#	print(exstr, values)
-#	cursor.execute(exstr, values)
-#
-#def print_table(cursor,table_name):
-#	print(cursor.execute('select * from %s' % table_name).fetchall())
-#
-#def commit_changes(connection):
-#	connection.commit()
-#
-#def open_connection_to_database(name):
-#	if os.path.isfile(name):
-#		os.rename(name,name+'.backup')
-#	connection=sqlite3.connect(name)
-#	cursor=connection.cursor()
-#	return connection,cursor
-#
-#def close_connection_to_database(connection):
-#	connection.close()
-#
-#class DatabaseGeneratorUI(cmd.Cmd):
-#	'''User Interface for BTPP Database Generator'''
-#	prompt='dg> '
-#	intro='Database Generator for BTPP website\nRun this only when the database needs to be entirely regenerated\nFor a list of commands, try entering \'help\''
-#	def do_greet(self, line=''):
-#		print('Database Generator for BTPP website\n')
-#		print('Run this only when the database needs to be entirely regenerated')
-#
-#	def do_EOF(self,line=''):
-#		print('\nQuitting...')
-#		return self.do_exit()
-#	def do_exit(self,line=''):
-#		return True
-#	def postloop(self):
-#		print()
-#	def cmdloop(self):
-#		try:
-#			cmd.Cmd.cmdloop(self)
-#		except KeyboardInterrupt as e:
-#			self.do_EOF()
-#
-#DatabaseGeneratorUI().cmdloop()
-#exit(0)
-
-#connection,cursor=open_connection_to_database(database_name)
-#create_table(cursor, 'pages',OrderedDict([('page_name','text'),('element_name','text'), ('element_value','text')]))
-#add_element_to_table(cursor,'pages',['base', 'logo_text','Birmingham Trust for <br/> Psychoanalytic Psychotherapy'])
-#add_element_to_table(cursor,'pages',['base', 'logo_image','nope'])
-#add_element_to_table(cursor,'pages',['home', 'title','home'])
-#
-#create_table(cursor, 'menu',OrderedDict([('name','text'),('url','text'), ('content','text')]))
-#add_element_to_table(cursor,'menu',['home','/btpp','home'])
-#
-#create_table(cursor, 'users',OrderedDict([('user_name','text'),('password','password')]))
-#add_element_to_table(cursor,'users',['kevin','216cb14fe707daefb5c630e14d2c87b25922489457d176dac00dd32e32a9c3f48ef7c8682cff9d7d9a6c1759f622d9af262334b06f98e19f86622a26bb435dfb'])
-#
-#commit_changes(connection)
-#close_connection_to_database(connection)
+		if not self.find_state(user_id):
+			self.__cursor.execute('insert into users values (?,0)', (user_id,))
+			self.__connection.commit()
+	def change_state(self,user_id,change):
+		state=self.find_state(user_id)
+		if not state:
+			raise Exception('changing state of user_id that cannot be found')
+		for c in change.split(','):
+			(index,value)=c.split('=')
+			state[index]=value
+		return state
+	def update_state(self,user_id,new_state):
+		self.__cursor.execute('update users set state=? where user_id=?', (state,user_id))	
+		self.__connection.commit()
+	def find_command(self,command):		
+		return self.__cursor.execute('select interaction_ids from commands where command=?', (command,)).fetchall()
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.4
 
 import twitter_interface as ti
-import user_interact as ui
+from random import choice
 
 if __name__=="__main__":
 	import sys
@@ -13,17 +13,20 @@ if __name__=="__main__":
 	if not os.path.isfile(database_path):
 		print("\x1b[31;1mError\x1b[0m: could not find supplied database \""+database_path+"\"")
 		exit(1)
-
-	
-	(initial_state, worlds)=ui.load(database_path)	
-
-	for (tweet_id,tweet_text) in ti.get_new_tweets():
-		if world:
-			exists=False
-			for room in world: 	
-				interaction=room.interact(tweet_text)	
-				if interaction:
-					exists=True
-			if exists: ti.dm_tweet_to_admins(tweet_id)	
-		else:
-			ti.dm_tweet_to_admins(tweet_id, "empty world")
+	database=Database(database_path)
+			
+	while(True):
+		for (tweet_id,tweet_text) in ti.get_new_tweets():
+			results=database.find_command(tweet_text)
+			if results is None:
+				if database.is_user(user):
+					ti.dm_tweet_to_admins(tweet_id)	
+				else:	
+					continue	
+			(response,change)=choice(results)
+			new_state=database.change_state(user,change)	
+			if new_state == '0':
+				database.add_user(user)
+			else:
+				database.update_state(user,new_state)
+			tweet(response)
